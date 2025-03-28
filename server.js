e91e3fccd02426b246b6535b76b3d6eb4419124d
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-03-28 16:36:49 +0100

    add cors middleware and update package.json

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const port = 3003;
 const postsRouter = require("./routers/posts");
 const errorsHandler = require("./middlewares/serverError");
 const notFound = require("./middlewares/error_404")
 
 
+app.use(express.static('public'))
 app.use(express.json());
-
+app.use(cors({
+    origin: 'http://localhost:5173'
+}))
 /* listening */
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 
 });
 
 /* homepage root */
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send(`Welcome on our server`)
 });
 
@@ -24,7 +28,7 @@ app.get('/',(req,res)=>{
 app.use('/api/v1/posts', postsRouter);
 
 
-app.post('/',(req, res)=>{
+app.post('/', (req, res) => {
     console.log(req.body)
 })
 
